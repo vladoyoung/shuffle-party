544ff2573b0fa2676158b4378cfc462a24ac121c
Vlad Nikolov <vladoyoung@gmail.com>
2024-03-09 02:27:58 +0200

    Small mobile usability fixes

--- src/lib/Layout.svelte
+++ src/lib/Layout.svelte
@@ -3,7 +3,7 @@
     import { Toaster } from '$lib/components/ui/sonner'
 </script>
 
-<div class="align-center container flex min-h-screen flex-col justify-center">
+<div class="align-center container flex flex-col justify-center overflow-hidden min-h-svh">
     <slot />
     <Toaster />
 </div>
